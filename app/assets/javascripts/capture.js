85f6ef7c3e22ec1c446e9b3876145ee0897d5853
Jessica Tran <kryx11@gmail.com>
2017-03-04 13:37:46 +1100

    checking

--- app/assets/javascripts/capture.js
+++ app/assets/javascripts/capture.js
@@ -52,23 +52,20 @@
 
      imageLoader.addEventListener('change', handleImage, false);
 
-      clearphoto();
+      // clearphoto();
 
     }
 
     function handleImage(e){
-
-
       var reader = new FileReader();
       reader.onload = function(event){
         var img = new Image();
-
-          img.onload = function(){
-              canvas.width = img.width;
-              canvas.height = img.height;
-              ctx.drawImage(img,0,0);
-          }
-          img.src = event.target.result;
+        img.onload = function(){
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img,0,0);
+        }
+        img.src = event.target.result;
       }
       reader.readAsDataURL(e.target.files[0]);
     }
